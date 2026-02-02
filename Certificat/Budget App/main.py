ed34a010eae06da2a0518a2299718c08bb3a0400
Madjid <madjid.boudjlida20@gmail.com>
2026-02-02 17:32:25 +0100

    modified:   Certificat/Budget App/main.py

--- Certificat/Budget App/main.py
+++ Certificat/Budget App/main.py
@@ -26,13 +26,12 @@ class Category:
         return False if amount > self.get_balance() else True
         
     def __str__(self):
-        x = (30 - len(self.name)) // 2
         text = f"{self.name.title():*^30}\n"
         for dico in self.ledger:
-            description = dico['description']
+            description = dico['description'][:23]
             amount = dico['amount']
             text += f"{description:<23}{amount:>7.2f}\n"    
-        text += f"Total: {self.get_balance():.2f}"
+        text += f"Total: {self.get_balance()}"
         return text
 
 
@@ -42,28 +41,26 @@ def create_spend_chart(categories: list):
     spendings = {}
 
     for categorie in categories:
-        spendings[categorie.name.capitalize()] = 0
+        spendings[categorie.name] = 0
         for ledg in categorie.ledger:
             if ledg['amount'] < 0:
-                spendings[categorie.name.capitalize()] += -ledg['amount']
+                spendings[categorie.name] += -ledg['amount']
 
     total_spending = sum(spendings.values())
 
     # ------spendings by total spending----
     for key in spendings.keys():
-        #print(f"{key:23}: {spendings[key]}")
-        val = round((spendings[key] / total_spending)*100, -1)
-        #print(f"{'val':23}: {val}")
+        val = (spendings[key] / total_spending)*100
         spendings.update([(key, val)])
-    
-    print(f"{'Spendings Values':<23}{spendings.values()}\n")
-    
+        
     chart = title
     for i in range(100, -1, -10):
         chart += f"\n{i:3}| "
         for key, value in spendings.items():
-            if value >= i:
+            if value >= round(i, -1):
                 chart += 'o  '
+            else:
+                chart += '   '
                 
     chart += f"\n{' '*4}{'-'*(len(spendings)*3+1)}"
     
@@ -73,7 +70,7 @@ def create_spend_chart(categories: list):
         chart += '\n'+ ' '*5
         for name in spendings.keys():
             if i <= len(name) - 1:
-                chart += f"{name[i]}  "
+                chart += f"{name[i]:3}"
             else:
                 chart += '   '
     return chart
@@ -89,4 +86,4 @@ c2.withdraw(800.34, 'withdraw')
 c2.withdraw(600.82, 'second withdraw')
 
 print(c1)
-#print(create_spend_chart([c1, c2]))
+print(create_spend_chart([c1, c2]))
